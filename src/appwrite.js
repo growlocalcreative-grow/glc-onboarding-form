37dd6466a67f572c0bb01aae77d65b12c8f8479b
growlocalcreative-grow <growlocalcreative@gmail.com>
2026-03-24 10:00:45 -0700

    So many eidts!

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,16 +1,19 @@
 import { Client, Databases, ID } from 'appwrite';
 
+// 1. Initialize the SDK
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
     .setProject('69c1aefe0001cc52eb04'); 
 
+// 2. Initialize the Database service
 const databases = new Databases(client);
 
+// 3. Export the submission function
 export const submitQuestionnaire = async (data) => {
     try {
         return await databases.createDocument(
-            '69c21463003b58b5b871', 
-            'submissions',         
+            '69c21463003b58b5b871', // Database ID
+            'submissions',          // Collection ID
             ID.unique(),
             data
         );
